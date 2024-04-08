bdef91807ae49ed16c34b93b6e1c2e27a526bc24
yang aaa <yangfancheng1@gmail.com>
2024-04-07 22:53:53 -0400

    Yangfan-0002: Add aop logging

--- src/main/java/io/github/yangfan/core/foo/FooService.java
+++ src/main/java/io/github/yangfan/core/foo/FooService.java
@@ -1,5 +1,6 @@
 package io.github.yangfan.core.foo;
 
+import io.github.yangfan.core.common.logging.LogExecutionTime;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ public class FooService {
 
     private final FooClient fooClient;
 
+    @LogExecutionTime
     public FooBarResponse getFoo(String userId) {
         val fooResponse = fooClient.getFoo(userId);
         return new FooBarResponse(String.format("%s, %s", fooResponse.getFieldA(), fooResponse.getFieldB()));
