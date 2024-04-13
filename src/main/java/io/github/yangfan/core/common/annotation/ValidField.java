46367831d5c882b9abf806ecf7255beb2f663c3b
yang aaa <yangfancheng1@gmail.com>
2024-04-13 10:33:17 -0400

    Yangfan-0004: Enable validation

---
+++ src/main/java/io/github/yangfan/core/common/annotation/ValidField.java
@@ -0,0 +1,28 @@
+package io.github.yangfan.core.common.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@NotBlank
+@Size(min = 1, max = 32)
+@Target({FIELD, PARAMETER})
+@Retention(RUNTIME)
+@Constraint(validatedBy = {})
+@Documented
+public @interface ValidField {
+    String message() default "Value length must be between 1 and 32, inclusively";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
